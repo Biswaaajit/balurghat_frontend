957b3047479cfebd0d2afe55a732a42cfc642f45
Biswajeet Barik <biswajeetbarik3936@gmail.com>
2025-07-02 19:45:30 +0530

    changed something

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <div className=" sticky top-0 z-50 bg-white shadow-md w-full flex items-center justify-between py-3 px-6">
+    <div className=" sticky top-0 z-50 bg-red-600 shadow-md w-full flex items-center justify-between py-3 px-6">
       <Link to="/">
-        <img src="/logo.png" className="w-[10rem]" />
+        <img src="/logo.png" className="w-[40rem]" />
       </Link>
       <Link
         className="w-fit bg-slate-100/50 border text-black hover:bg-slate-950 hover:text-white flex justify-center items-center gap-0.5 px-4 py-1 rounded-lg font-semibold"
